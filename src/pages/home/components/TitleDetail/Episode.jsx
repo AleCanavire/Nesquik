66da2603afb459b518f95bf695334371536fb567
AleCanavire <ale.canavire1@gmail.com>
2023-06-02 00:07:32 -0300

    Image Load Fix

--- src/pages/home/components/TitleDetail/Episode.jsx
+++ src/pages/home/components/TitleDetail/Episode.jsx
@@ -26,11 +26,13 @@ function Episode({ season, episode, type, id }) {
         {episode}
       </div>
       <div className="title-card-image">
-        <img
-          src={`https://image.tmdb.org/t/p/w300${episodeData?.still_path}`}
-          alt={episodeData?.name}
-          loading="lazy"
-        />
+        { episodeData?.still_path &&
+          <img
+            src={`https://image.tmdb.org/t/p/w300${episodeData?.still_path}`}
+            alt={episodeData?.name}
+            loading="lazy"
+          />
+        }
         <div className="title-play-icon">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="titleCard-playSVG" data-name="Play"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg>
         </div>
