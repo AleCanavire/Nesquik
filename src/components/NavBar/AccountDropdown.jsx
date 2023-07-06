67b2f37a8cdc44e5da3ebcc8d8fc8ac07a0a9b80
AleCanavire <ale.canavire1@gmail.com>
2023-07-05 21:28:36 -0300

    Link to Profiles Manage

--- src/components/NavBar/AccountDropdown.jsx
+++ src/components/NavBar/AccountDropdown.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { UserAuth } from '../../context/AuthContext';
+import { Link } from 'react-router-dom';
 
 function AccountDropdown({ onHideDropdown }) {
-  const { user, activeProfile, logOut } = UserAuth();
+  const { user, activeProfile, setActiveProfile, logOut } = UserAuth();
   const [showDropdown, setShowDropdown] = useState(false);
   const timer = useRef();
 
@@ -47,10 +48,10 @@ function AccountDropdown({ onHideDropdown }) {
         </ul>
         <ul className="account-links">
           <li className="sub-menu-item">
-            <div className="item-link">
+            <Link className="item-link" to="/profiles/manage" onClick={()=> setActiveProfile(null)}>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="Hawkins-Icon Hawkins-Icon-Standard" data-name="Edit"><path fillRule="evenodd" clipRule="evenodd" d="M22.2071 7.79285L15.2071 0.792847L13.7929 2.20706L20.7929 9.20706L22.2071 7.79285ZM13.2071 3.79285C12.8166 3.40232 12.1834 3.40232 11.7929 3.79285L2.29289 13.2928C2.10536 13.4804 2 13.7347 2 14V20C2 20.5522 2.44772 21 3 21H9C9.26522 21 9.51957 20.8946 9.70711 20.7071L19.2071 11.2071C19.5976 10.8165 19.5976 10.1834 19.2071 9.79285L13.2071 3.79285ZM17.0858 10.5L8.58579 19H4V14.4142L12.5 5.91417L17.0858 10.5Z" fill="currentColor"></path></svg>
               <span className="sub-menu-name">Administrar perfiles</span>
-            </div>
+            </Link>
           </li>
           <li className="sub-menu-item">
             <div className="item-link">
